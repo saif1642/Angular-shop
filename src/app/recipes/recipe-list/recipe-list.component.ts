5059fe15439d7f89114e8a9032e0ca217aeb0f8b
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-07 17:32:11 +0600

    recipe list functionality added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../../recipes/recipe.model";
 
 @Component({
@@ -7,19 +7,25 @@ import { Recipe } from "../../recipes/recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      "This is test",
-      "dummy heading",
+      "This is Recipe One",
+      "dummy Recipe text",
       "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"
     ),
     new Recipe(
-      "This is test",
-      "dummy heading",
-      "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"
+      "This is Recipe Two",
+      "Recipe text",
+      "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_960_720.jpg"
     )
   ];
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
