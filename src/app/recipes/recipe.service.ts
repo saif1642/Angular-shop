fe8f1ce36fb7f5c6925c2918911898b25c4697f4
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-08 18:55:40 +0600

    adding ingredient to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,20 @@
 import { Recipe } from "../recipes/recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       "This is Recipe One",
       "dummy Recipe text",
-      "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"
+      "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg",
+      [new Ingredient("Meat", 2), new Ingredient("Potato", 5)]
     ),
     new Recipe(
       "This is Recipe Two",
       "Recipe text",
-      "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_960_720.jpg"
+      "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_960_720.jpg",
+      [new Ingredient("Salt", 2), new Ingredient("Mirchi", 5)]
     )
   ];
 
