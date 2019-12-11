3103ee6cb78251dbe64f678ac52985c0fe8ce1a7
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-11 13:57:21 +0600

    transform fetching data using pipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,21 +6,21 @@ import { Subject } from "rxjs";
 @Injectable()
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      "This is Recipe One",
-      "dummy Recipe text",
-      "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg",
-      [new Ingredient("Meat", 2), new Ingredient("Potato", 5)]
-    ),
-    new Recipe(
-      "This is Recipe Two",
-      "Recipe text",
-      "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_960_720.jpg",
-      [new Ingredient("Salt", 2), new Ingredient("Mirchi", 5)]
-    )
-  ];
-
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "This is Recipe One",
+  //     "dummy Recipe text",
+  //     "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg",
+  //     [new Ingredient("Meat", 2), new Ingredient("Potato", 5)]
+  //   ),
+  //   new Recipe(
+  //     "This is Recipe Two",
+  //     "Recipe text",
+  //     "https://cdn.pixabay.com/photo/2016/09/15/19/24/salad-1672505_960_720.jpg",
+  //     [new Ingredient("Salt", 2), new Ingredient("Mirchi", 5)]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private shoppingService: ShoppingService) {}
 
   getRecipes() {
