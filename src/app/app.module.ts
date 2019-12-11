1ce44288f5fa7fdd5ffe985ff55cad296beab724
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-11 17:41:06 +0600

    auth start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeService } from "./recipes/recipe.service";
 import { DataStorageService } from "./shared/data-storage.service";
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { DataStorageService } from "./shared/data-storage.service";
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
