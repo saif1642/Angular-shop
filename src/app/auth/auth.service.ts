b5b6d977bea6edbfad591899164e8e2455200e8d
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-15 18:33:35 +0600

    auto login and logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,6 +17,7 @@ interface AuthResponseData {
 @Injectable({ providedIn: "root" })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
   constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
@@ -36,10 +37,37 @@ export class AuthService {
             resData.email,
             resData.localId,
             resData.idToken,
-            resData.expiresIn
+            parseInt(resData.expiresIn)
           );
         })
       );
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const tokenExpireDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(tokenExpireDuration * 1000);
+      this.router.navigate(["/recipes"]);
+    }
   }
 
   login(email: string, password: string) {
@@ -59,7 +87,7 @@ export class AuthService {
             resData.email,
             resData.localId,
             resData.idToken,
-            resData.expiresIn
+            parseInt(resData.expiresIn)
           );
         })
       );
@@ -68,17 +96,30 @@ export class AuthService {
   logout() {
     this.user.next(null);
     this.router.navigate(["/auth"]);
+    localStorage.removeItem("userData");
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuthentication(
     email: string,
     id: string,
     token: string,
-    expiresIn: string
+    expiresIn: number
   ) {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, id, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 
   private handleErrors(errorRes: HttpErrorResponse) {
